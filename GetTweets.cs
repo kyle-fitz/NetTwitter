e5703a3f58b2349c4b713fd2e4f40fafb2ae79dd
kyle-fitz <kylefitz621@gmail.com>
2020-12-21 20:44:08 -0500

    Port to Core

---
+++ GetTweets.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using LinqToTwitter;
+using System.Text.RegularExpressions;
+
+namespace NetTwitter
+{
+    public class GetTweets
+    {
+        static void PrintCleanTweetsofHyperLink(List<Status> tweets)
+        {
+
+            if (tweets != null)
+                tweets.ForEach(tweet =>
+                {
+                    string pattern = "https://";
+
+                    if (tweet.FullText.Contains(pattern))
+                    {
+
+                        string resultTweet = Regex.Replace(tweet.FullText, @"http[^\s]+", "");
+
+                    Console.WriteLine(
+                        "This Tweet had a hyperlink that was removed. {0}", resultTweet);
+                    }else if (!tweet.FullText.Contains(pattern)){
+                        Console.WriteLine(
+                            "Tweet with no hyperlink found: {0}", tweet.FullText);    
+                    };
+                });
+        }
+        
+        //removes the tweets with https -- want to strip the links from the tweet...
+        static void PrintTweetsResults(List<Status> tweets)
+        {
+            if (tweets != null)
+                tweets.ForEach(tweet =>
+                {
+                    if (tweet != null && tweet.User != null && !tweet.Text.Contains("https://"))
+                        Console.WriteLine(
+                            "Username: {0} && Tweet: {1}", tweet.User.ScreenNameResponse, tweet.Text);
+                });
+        }
+
+        //Donald Trump tweets to list 
+        public static async Task RunUserTimelineQueryAsync(TwitterContext twitterCtx)
+        {
+            var tweets =
+                await
+                (from tweet in twitterCtx.Status
+                 where tweet.TweetMode == TweetMode.Extended &&
+                        tweet.Type == StatusType.User &&
+                       tweet.ScreenName == "realDonaldTrump"
+                 select tweet)
+                .ToListAsync();
+
+            PrintCleanTweetsofHyperLink(tweets);
+            //PrintTweetsResults(tweets);
+        }
+
+        //Search for Donald Trump -- Could be useful for searching for hot words.
+        public static async Task NewAsyncReturnSearch(TwitterContext twitterCtx)
+        {
+
+            var searchResponse =
+                await
+                (from search in twitterCtx.Search
+                 where search.Type == SearchType.Search &&
+                       search.Query == "\"Donald Trump\""
+                 select search)
+                .SingleOrDefaultAsync();
+
+            if (searchResponse != null && searchResponse.Statuses != null)
+                searchResponse.Statuses.ForEach(tweet =>
+                    Console.WriteLine(
+                        "User: {0}, Tweet: {1}",
+                        tweet.User.ScreenNameResponse,
+                        tweet.Text));
+        }
+
+        //Donald Trump last Tweet 
+        public static async Task ShowUserDetailsAsync(TwitterContext twitterCtx)
+        {
+            var user =
+                await
+                (from tweet in twitterCtx.User
+                 where tweet.Type == UserType.Show &&
+                       tweet.ScreenName == "realDonaldTrump"
+                 select tweet)
+                .SingleOrDefaultAsync();
+
+            if (user != null)
+            {
+                var name = user.ScreenNameResponse;
+                var lastStatus =
+                    user.Status == null ? "No Status" : user.Status.Text;
+
+                Console.WriteLine();
+                Console.WriteLine(
+                    "Name: {0}, Last Tweet: {1}\n", name, lastStatus);
+            }
+        }
+
+        //Returns raw data searching for unencodedStatus
+        public static async Task PerformSearchRawAsync(TwitterContext twitterCtx)
+        {
+            string unencodedStatus = "realDonaldTrump";
+            string encodedStatus = Uri.EscapeDataString(unencodedStatus);
+            string queryString = "search/tweets.json?q=" + encodedStatus;
+
+            var rawResult =
+                await
+                (from raw in twitterCtx.RawQuery
+                 where raw.QueryString == queryString
+                 select raw)
+                .SingleOrDefaultAsync();
+
+            if (rawResult != null)
+                Console.WriteLine(
+                    "Response from Twitter: \n\n" + rawResult.Response);
+        }
+
+        public static async Task RunHomeTimelineQueryAsync(TwitterContext twitterCtx)
+        {
+            var tweets =
+                await
+                (from tweet in twitterCtx.Status
+                 where tweet.Type == StatusType.Home
+                 select tweet)
+                .ToListAsync();
+
+            PrintTweetsResults(tweets);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using LinqToTwitter;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NetTwitter
+{
+    internal class Program
+    {
+        // public static void Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            var tweets = new GetTweets();
+            
+            var auth = new SingleUserAuthorizer
+            {
+
+                CredentialStore = new InMemoryCredentialStore()
+                {
+                    
+                    ConsumerKey = ConfigurationSettings.AppSettings["consumerKey"],
+                    ConsumerSecret = ConfigurationSettings.AppSettings["consumerSecret"],
+                    OAuthToken = ConfigurationSettings.AppSettings["accessToken"],
+                    OAuthTokenSecret = ConfigurationSettings.AppSettings["accessTokenSecret"]
+                }
+
+            };
+            
+            //FileStream > Stream Writer > Check Exists > Delte or Create > Open > Get Console > Write Console > Close
+            FileStream fileStream;
+            StreamWriter writer;
+            TextWriter oldConsole = Console.Out;
+            
+            //set the filepath to the output of the console.
+            var filePath = "";
+            var fileName = "TweetList.txt";
+            var fullPath = filePath + fileName;
+
+            if (File.Exists(Path.Combine(filePath, fileName)))
+            {
+                File.Delete(Path.Combine(filePath, fileName));
+                Console.WriteLine("File Deleted... Preparing output file...");
+            }
+
+            try
+            {
+
+                fileStream = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(fileStream);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Cannot open TweetList.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.SetOut(writer);
+
+            var twitterCtx = new TwitterContext(auth);
+            Console.WriteLine("Gathering tweets from Donald Trump");
+            Console.WriteLine("TimeStamp: " + DateTime.Now);
+            
+            await GetTweets.ShowUserDetailsAsync(twitterCtx);
+            
+            await GetTweets.RunUserTimelineQueryAsync(twitterCtx);
+            
+            await GetTweets.RunHomeTimelineQueryAsync(twitterCtx);
+
+
+            Console.SetOut(oldConsole);
+
+            writer.Close();
+            oldConsole.Close();
+            
+        }
+    }
+}
